4813fcfcf7ecad5e9f8d22e1572710fa3b6a15c5
JangMinSeong <hs0765@naver.com>
2021-06-21 16:52:33 +0900

    21.06.21 Main 제작 및 작업 분배

--- app/src/main/java/com/example/photodaily/MainActivity.kt
+++ app/src/main/java/com/example/photodaily/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.photodaily
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var intent = Intent(this, TabNavigator::class.java)
+        startActivity(intent)
     }
 }
